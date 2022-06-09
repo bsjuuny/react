6c02ac68499eecb04da58d8657ac4cbfe160b88e
v215018 <v215018@cusmo.cup>
2022-06-09 17:36:02 +0900

    테스트 코드 작성

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,6 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import styled from '@emotion/styled'
+import './App.css';
 import useWatchLocation from './useWatchLocation'
 import useLocalStorage from './useLocalStorage'
+import axios from 'axios'
 
 export default function Map() {
   let getLat: number = 0
@@ -13,20 +16,23 @@ export default function Map() {
   }
 
   const script = document.createElement('script')
+  let coords: string = ''
+  coords = '[new Tmapv2.LatLng(' + getLat + ', ' + getLng + ')]'
+
   script.innerHTML = `
     var setMap = null;
     function initTmap() {
       setMap = new Tmapv2.Map('TMapApp', {
           center: new Tmapv2.LatLng(`+ getLat +`, `+ getLng +`),
           width: '100%',
-          height: '100%',
+          height: '400px',
           zoom: 16
       });
       addMarkerAni(Tmapv2.MarkerOptions.ANIMATE_FLICKER);
     }
 
     var markers = [];
-    var coords = [new Tmapv2.LatLng(`+ getLat + `, ` + getLng +`)];
+    var coords = `+ coords +`;
 
     function addMarkerAni(aniType) {
       var coordIdx = 0;
@@ -56,9 +62,75 @@ export default function Map() {
   script.type = 'text/javascript'
   script.async = true
 
-  if (getLat && getLng) {
-    document.head.appendChild(script)
-  }
+  let count: number = 0
+
+  const [rendered, setRendered] = useState(false);
+  useEffect(() => {
+    if (!rendered && getLat && getLng) {
+      document.head.appendChild(script)
+      setRendered(true)
+    }
+  }, [getLat, getLng, script])
+
+  const [inputs, setInputs] = useState({
+    search: ''
+  });
+  const { search } = inputs;
+  const [states, setState] = useState({
+    isLoading: true,
+    result: [],
+    meta: [],
+  });
+
+  const onChange = (e: any) => {
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value // name 키를 가진 값을 value 로 설정
+    });
+  };
+
+  const getSearchMovie = async () => {
+    // const ID_KEY: string = '06KgZ7pRRC_FRihC_6ef';
+    // const SECRET_KEY: string = 'VMa_XW3Qie';
+    const KAKAO_KEY: string = 'KakaoAK d49e0f1eba3e8b2b6af118762ce8375a'
+    const search_radius: number = 1600
+    const search_size: number = 15
+    let search_page: number = 1
+    const search: string = inputs.search
+    try {
+      if (search === "") {
+        setState({result: [], isLoading: false, meta: []})
+      } else {
+        // const { data: { items }} = await axios.get('/v2/local/search/keyword.json', {
+        const response = await axios.get('/v2/local/search/keyword.json', {
+          params: {
+            query: search,
+            radius: search_radius,
+            size: search_size,
+            page: search_page,
+            x: getLng,
+            y: getLat,
+            // category_group_code: 'FD6',
+          },
+          headers: {
+              // 'X-Naver-Client-Id': ID_KEY,
+              // 'X-Naver-Client-Secret': SECRET_KEY
+              'Authorization': KAKAO_KEY,
+            }
+          });
+
+        console.log(response.data);
+        response.data.documents.map((value: any, index: number) => {
+          let getData = value["category_name"].split('>')
+          value["category_name_detail"] = getData[getData.length - 1].trim()
+        })
+        setState({ result: response.data.documents, isLoading: false, meta: response.data.meta });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const [store, setStore, clearStore] = useLocalStorage('location', { lat: getLat, lng: getLng })
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -68,25 +140,36 @@ export default function Map() {
   }
 
   return (
-    <div
-      id='TMapApp'
-      style={{
-        height: '100%',
-        width: '100%',
-        position: 'fixed',
-      }}
-    >
-      {/* <button onClick={handleClick}> */}
-      <button onClick={() => {
-        setStore('location' + window.localStorage.length, { lat: getLat, lng: getLng })
-      }}>
-        Click me {window.localStorage.length}
-      </button>
-      <button onClick={() => {
-        clearStore()
-      }}>
-        Clear
-      </button>
+    <div style={{padding: '20px', boxSizing: 'border-box'}}>
+      <div id='TMapApp' style={{width: '100%', height: '400px', margin: '0 0 20px'}}></div>
+      <div>
+        <button type="button" onClick={() => {
+            setStore('location' + window.localStorage.length, { lat: getLat, lng: getLng })
+          }}>
+          현재 위치 저장 {window.localStorage.length}
+        </button>
+        <button type="button" onClick={() => {
+          clearStore()
+        }}>
+          초기화
+        </button>
+        <input name="search" placeholder="검색어를 입력하세요" onChange={onChange} value={search} />
+        <button type="button" onClick={getSearchMovie}>검색</button>
+        { states.result.length === 0 && <p>검색결과 없음</p> }
+        <ul style={{ margin: '10px 0 0' }}>
+          {states.result.map((value, index) => (
+            <li style={{ padding: '5px 0 0 20px' }}>
+              <span key={ index }>
+                {value["place_name"]}
+                <sub style={{ display: 'inline-block', margin: '0 0 0 10px', fontSize: '12px', color: '#999', verticalAlign: '2px' }}>
+                  {value["category_name_detail"]}{" / "}{value["road_address_name"]}
+                </sub>
+              </span>
+              <button type="button" data-x={value["x"]} data-y={value["y"]} style={{ display: 'inline-block', margin: '0 0 0 10px', verticalAlign: '2px' }}>선택</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
